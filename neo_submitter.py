3e59b13b90f7e65e249147b656e9fd623d98cc20
wy <wingyungchan@gmail.com>
2018-01-23 23:12:55 +0000

    transferance has ocurred

--- neo_submitter.py
+++ neo_submitter.py
@@ -29,8 +29,8 @@ from neocore.KeyPair import KeyPair
 # Setup the smart contract instance
 # This is online voting v0.5
 smart_contract = SmartContract("ef254dc68e36de6a3a5d2de59ae1cdff3887938f ")
-wallet_hash = 'AYr1yJo42i58dU6kQX6j96ayCw13zhvjYv'
-wif = 'L5Cp8JMBuLQXvsY5Gijj7oPXkit9skMpsJu7ECyyrnvBmJcgGa7v'
+wallet_hash = 'AKgM1UKH1yaqWXZ24ro3woaxbkXnwGJPnP'
+#wif = 'L5Cp8JMBuLQXvsY5Gijj7oPXkit9skMpsJu7ECyyrnvBmJcgGa7v'
 Wallet = None
 
 
